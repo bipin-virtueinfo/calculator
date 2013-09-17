1d293e9bb33e950d5f4004c768abb330c21fccd0
Bipin Patel <bipin@virtueinfo.com>
2013-09-17 18:51:39 +0530

    Added numeric validation and display values

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -1,18 +1,19 @@
 class FrontController < ApplicationController
   def dashboard
-    @investment = (session[:id]) ? Investment.find(session[:id]) : Investment.new
+    @investment = Investment.new
     if request.post?
-        @investment = Investment.new(investment_params)
-        respond_to do |format|
-          if @investment.save
-            session[:id] = @investment.id
-            format.html { redirect_to dashboard_path, notice: "Return on Investment is: #{@investment.profit_per_square_feet}% with a profit of #{@investment.square_feet} per square feet" }
-            format.json {}
-          else
-            format.html { render action: 'dashboard' }
-            format.json { render json: @investment.errors, status: :unprocessable_entity }
-          end
+      @investment = Investment.new(investment_params)
+      respond_to do |format|
+        if @investment.save
+          flash[:notice] = "Return on Investment is: #{@investment.profit_per_square_feet}% with a profit of #{@investment.square_feet} per square feet"
+          @investment = Investment.new
+          format.html { render action: 'dashboard' }
+          format.json {}
+        else
+          format.html { render action: 'dashboard' }
+          format.json { render json: @investment.errors, status: :unprocessable_entity }
         end
+      end
     end
   end
 
